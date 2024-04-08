82269bcd394b3c6f369417c0ca044d6723b97119
asakura89 <flamboyan3no1a@gmail.com>
2024-04-09 03:57:23 +0700

    [New] Asp WebForm

---
+++ Stripped-WebForm/App_Start/RouteConfig.cs
@@ -0,0 +1,12 @@
+using System.Web.Routing;
+using Microsoft.AspNet.FriendlyUrls;
+
+namespace Stripped {
+    public static class RouteConfig {
+        public static void RegisterRoutes(RouteCollection routes) {
+            var settings = new FriendlyUrlSettings();
+            settings.AutoRedirectMode = RedirectMode.Permanent;
+            routes.EnableFriendlyUrls(settings);
+        }
+    }
+}
---
+++ Stripped-WebForm/Default.aspx.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Stripped {
+    public partial class _Default : Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
